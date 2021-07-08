8e699eac73e815d4dbf9dc6957c58a253dad2526
Yilma Estefanía Sora Albornoz <estefayiso@gmail.com>
2021-07-08 17:35:05 -0500

    V 0.3

--- client.js
+++ client.js
@@ -1,21 +1,27 @@
 var btn = document.getElementById("myBtn");
 btn.addEventListener("click", getQuote, false);
 
+
 function getQuote(){
     let xhttp = new XMLHttpRequest();
+    let xhttp2 = new XMLHttpRequest();
     var myUrl = "http://localhost:3000";
     let pName = document.getElementById("pName").value
     let pPrice = document.getElementById("pPrice").value
     xhttp.open("POST", myUrl);
+    
     xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    
     xhttp.send(JSON.stringify({ Nombre_Producto: pName, Precio_Producto: pPrice }))
-    xhttp.onreadystatechange = function() {
+
+    xhttp2.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            var text = document.createElement("h2");
-            text.innerHTML = xhttp.responseText;
-            document.body.appendChild(text)
+            const data = JSON.parse(xhttp2.responseText)   
+            var price = document.createElement("H2");
+            price.innerHTML = "Nombre: "+ data.name + " ----- " + "Precio: $" +data.price ;
+            document.body.appendChild(price)
         }
     };
-    xhttp.open("GET", "http://localhost:3000", true);
-    xhttp.send()
+    xhttp2.open("GET", "http://localhost:3000", true);
+    xhttp2.send()
 }
